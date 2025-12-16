0dd5951d48e58ae07180d3843d6af8dcbe77e5a4
GalaletsangM <preciousmafoko@gmail.com>
2025-12-16 15:37:45 -0500

    commit

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,9 @@
+var teacher = {
+    firstName: "Abel",
+    lastName: "Smith",
+    fullTimeEmployee: true,
+    yearsOfExperience: 16,
+    location: "South Africa",
+    occupation: "Engineer"
+};
+console.log(teacher);
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,14 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+
+const teacher: Teacher = {
+    firstName: "Abel",
+    lastName: "Smith",
+    fullTimeEmployee: true,
+    yearsOfExperience: 16,
+    location: "South Africa",
+    occupation: "Engineer"
+}
+
+console.log(teacher);
